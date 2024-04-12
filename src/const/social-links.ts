61708078b7c5aeda5ac686314dd4793288f6e064
psmyrdek <przemek.smyrdek@gmail.com>
2024-04-12 23:06:21 +0200

    feat: layout v1

---
+++ src/const/social-links.ts
@@ -0,0 +1,4 @@
+export const LINK_X = 'https://twitter.com/psmyrdek';
+export const LINK_YT = 'https://www.youtube.com/@RideTimePL';
+export const LINK_INSTA = 'https://www.instagram.com/ridetimepl';
+export const LINK_STRAVA = 'https://www.strava.com/athletes/5973229';
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,6 +3,10 @@ export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
     extend: {
+      colors: {
+        'ridetime-light': '#9ACDFA',
+        'ridetime-dark': '#1B4A7F'
+      },
       fontFamily: {
         main: ["Francy", "sans-serif"],
       },
